7e641e544861357655ded45f4694c07f778783b2
Frederic Jasmin <76606949+fredericjasmin@users.noreply.github.com>
2024-07-26 16:14:11 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Agrega evento de scroll al documento
+document.addEventListener("scroll", function () {
+    var scrollPosition = window.scrollY;
+    var sections = document.querySelectorAll(".content > div");
+    var navbarLinks = document.querySelectorAll("nav ul li a");
+
+    sections.forEach(function (section, index) {
+        var sectionTop = section.offsetTop;
+        var sectionHeight = section.offsetHeight;
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            navbarLinks.forEach(function (link) {
+                link.classList.remove("active");
+            });
+            navbarLinks[index].classList.add("active");
+        }
+    });
+});
+
+// Agrega evento de click a cada enlace del navbar
+document.querySelectorAll("nav ul li a").forEach(function (link) {
+    link.addEventListener("click", function (event) {
+        event.preventDefault();
+        var sectionId = link.getAttribute("href");
+        var section = document.querySelector(sectionId);
+        section.scrollIntoView({ behavior: "smooth" });
+        link.classList.add("active");
+        document.querySelectorAll("nav ul li a").forEach(function (otherLink) {
+            if (otherLink !== link) {
+                otherLink.classList.remove("active");
+            }
+        });
+    });
+});
+
+document.getElementById('copy-button').addEventListener('click', () => {
+    const email = document.getElementById('email').textContent;
+    navigator.clipboard.writeText(email).then(() => {
+        alert('Correo copiado al portapapeles!');
+    }, (err) => {
+        console.error('Error al copiar el correo: ', err);
+    });
+});
+
