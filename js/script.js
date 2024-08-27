f5024f6e4725f45cf8311861b7f3b16a37357cbd
Frederic Jasmin <76606949+fredericjasmin@users.noreply.github.com>
2024-08-26 20:22:28 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
-document.addEventListener("scroll", function () {
-    var scrollPosition = window.scrollY;
-    var sections = document.querySelectorAll(".content > div");
-    var navbarLinks = document.querySelectorAll("nav ul li a");
-
-    sections.forEach(function (section, index) {
-        var sectionTop = section.offsetTop;
-        var sectionHeight = section.offsetHeight;
-        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-            navbarLinks.forEach(function (link) {
-                link.classList.remove("active");
+window.addEventListener('scroll', () => {
+    const navbarHeight = document.querySelector('nav').offsetHeight;
+    document.querySelectorAll('section[id]').forEach(section => {
+        const sectionTop = section.offsetTop - navbarHeight;
+        const sectionHeight = section.offsetHeight;
+        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+            const currentLink = document.querySelector(`a[href="#${section.getAttribute('id')}"]`);
+            document.querySelectorAll('.nav-link').forEach(link => {
+                link.classList.remove('active');
             });
-            navbarLinks[index].classList.add("active");
+            currentLink.classList.add('active');
         }
+    });
+});
+
+
+document.querySelectorAll("nav ul li a").forEach(function (link) {
+    link.addEventListener("click", function () {
+        document.getElementById('click').checked = false;
     });
 });
 
@@ -29,6 +34,20 @@ document.querySelectorAll("nav ul li a").forEach(function (link) {
         });
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const navbarHeight = document.querySelector('nav').offsetHeight;
+        window.scrollTo({
+            top: targetElement.offsetTop - navbarHeight,
+            behavior: 'smooth'
+        });
+    });
+});
+
 
 document.getElementById('copy-button').addEventListener('click', () => {
     const email = document.getElementById('email').textContent;
