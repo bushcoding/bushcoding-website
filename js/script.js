c630d11990259bd06a7183ba043a83583286fbf7
Frederic Jasmin <bushcoding@gmail.com>
2024-10-18 11:36:18 -0600

    update

--- js/script.js
+++ js/script.js
@@ -1,57 +1,23 @@
-window.addEventListener('scroll', () => {
-    const navbarHeight = document.querySelector('nav').offsetHeight;
-    document.querySelectorAll('section[id]').forEach(section => {
-        const sectionTop = section.offsetTop - navbarHeight;
-        const sectionHeight = section.offsetHeight;
-        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
-            const currentLink = document.querySelector(`a[href="#${section.getAttribute('id')}"]`);
-            document.querySelectorAll('.nav-link').forEach(link => {
-                link.classList.remove('active');
-            });
-            currentLink.classList.add('active');
-        }
-    });
-});
+document.querySelectorAll("nav ul li a").forEach(link => {
+    link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const sectionId = link.getAttribute("href");
+        const section = document.querySelector(sectionId);
+        
+        const navbarHeight = document.querySelector('nav').offsetHeight;
+        window.scrollTo({
+            top: section.offsetTop - navbarHeight,
+            behavior: 'smooth'
+        });
 
-
-document.querySelectorAll("nav ul li a").forEach(function (link) {
-    link.addEventListener("click", function () {
         document.getElementById('click').checked = false;
     });
 });
 
-document.querySelectorAll("nav ul li a").forEach(function (link) {
-    link.addEventListener("click", function (event) {
-        event.preventDefault();
-        var sectionId = link.getAttribute("href");
-        var section = document.querySelector(sectionId);
-        section.scrollIntoView({ behavior: "smooth" });
-        link.classList.add("active");
-        document.querySelectorAll("nav ul li a").forEach(function (otherLink) {
-            if (otherLink !== link) {
-                otherLink.classList.remove("active");
-            }
-        });
-    });
-});
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-        const navbarHeight = document.querySelector('nav').offsetHeight;
-        window.scrollTo({
-            top: targetElement.offsetTop - navbarHeight,
-            behavior: 'smooth'
-        });
-    });
-});
-
-
 document.getElementById('copy-button').addEventListener('click', () => {
     const email = document.getElementById('email').textContent;
     const copyIcon = document.getElementById('copy-icon');
+
     navigator.clipboard.writeText(email).then(() => {
         copyIcon.classList.remove('fa-copy');
         copyIcon.classList.add('fa-check');
